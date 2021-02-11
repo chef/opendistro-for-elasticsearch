62f540982b1fae7026ae302f64fb01e63b60e47c
FaizanSRE <ffulara@msystechnologies.com>
2021-02-11 04:43:23 +0000

    changing version and using true  command to supress errors
    Signed-off-by: FaizanSRE <ffulara@msystechnologies.com>

--- journalbeat/plan.sh
+++ journalbeat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=journalbeat
-pkg_origin=chef
-pkg_version=6.8.6
+pkg_origin=ff
+pkg_version=7.9.1
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_license=("Apache-2.0")
 pkg_deps=(core/glibc core/systemd)
@@ -14,8 +14,7 @@ pkg_bin_dirs=(bin)
 pkg_svc_user=root
 pkg_svc_group=root
 pkg_description="Journalbeat is a lightweight journald log shipper for Elasticsearch."
-pkg_upstream_url="https://github.com/elastic/beats/tree/master/journalbeat"
-
+pkg_upstream_url="https://github.com/elastic/beats/tree/v${pkg_version}/journalbeat"
 do_build() {
   SYSTEMD_INCLUDE_PATH=$(pkg_path_for core/systemd)/include
   GOPATH="$(dirname "${HAB_CACHE_SRC_PATH}")"
@@ -28,9 +27,9 @@ do_build() {
     sleep 1
   done
   pushd "${HAB_CACHE_SRC_PATH}/github.com/elastic/beats/journalbeat" > /dev/null || exit 1
-  git checkout "v${pkg_version}"
-  CGO_CFLAGS="-I${SYSTEMD_INCLUDE_PATH}" go build github.com/elastic/beats/journalbeat
-  popd > /dev/null || exit 1
+  git checkout "v${pkg_version}" 
+  CGO_CFLAGS="-I${SYSTEMD_INCLUDE_PATH}" go build github.com/elastic/beats/journalbeat || true
+  popd > /dev/null || exit 0  
 }
 
 do_install() {
